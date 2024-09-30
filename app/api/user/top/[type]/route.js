e39f3b681108760773a9afa38e6777260c043067
Capucine Trossat <ctrossat.dev@gmail.com>
2024-09-30 16:27:19 +0200

    adjusted spotify api call

--- app/api/user/top/[type]/route.js
+++ app/api/user/top/[type]/route.js
@@ -3,9 +3,10 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route.js"
 
 export async function GET(request, {params}) {
   const session = await getServerSession(authOptions);
+  const searchParams = request.nextUrl.search
 
   if(session) {
-    const response = await fetch(`${process.env.SPOTIFY_API_URL}/me/top/${params.type}`, {
+    const response = await fetch(`${process.env.SPOTIFY_API_URL}/me/top/${params.type+searchParams}`, {
       headers: {
         'Authorization' : `Bearer ${session.accessToken}`
       }
